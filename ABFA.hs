58b5abc6b943db6af376f40a8ccc4935192a8743
Vince Coghlan <vince.coghlan@colorado.edu>
2016-10-25 18:27:46 -0600

    initial commit

---
+++ ABFA.hs
@@ -0,0 +1,121 @@
+import qualified Graphics.UI.GLFW as GLFW
+import Graphics.Rendering.OpenGL.Raw
+import Graphics.Rendering.GLU.Raw
+import Data.Bits ( (.|.) )
+import System.Exit ( exitWith, ExitCode(..) )
+import Control.Monad ( forever )
+import Data.IORef ( IORef, newIORef, readIORef, writeIORef )
+import Foreign ( withForeignPtr, plusPtr, peek, alloca )
+import qualified Data.ByteString.Internal as BSI
+import Util ( Image(..), bitmapLoad )
+import Paths
+
+initGL :: GLFW.Window -> IO GLuint
+initGL win = do
+  glEnable gl_TEXTURE_2D
+  glShadeModel gl_SMOOTH -- enables smooth color shading
+  glClearColor 0 0 0 0 -- Clear the background color to black
+  glClearDepth 1 -- enables clearing of the depth buffer
+  glEnable gl_DEPTH_TEST
+  glDepthFunc gl_LEQUAL -- type of depth test
+  glHint gl_PERSPECTIVE_CORRECTION_HINT gl_NICEST
+  (w,h) <- GLFW.getFramebufferSize win
+  resizeScene win w h
+  loadGLTextures
+
+loadGLTextures :: IO GLuint
+loadGLTextures = do
+  fp <- getDataFileName "NeHe.bmp"
+  putStrLn $ "loading texture: " ++ fp
+  Just (Image w h pd) <- bitmapLoad fp
+  putStrLn $ "Image width  = " ++ show w
+  putStrLn $ "Image height = " ++ show h
+  tex <- alloca $ \p -> do
+    glGenTextures 1 p
+    peek p
+  let (ptr, off, _) = BSI.toForeignPtr pd
+  withForeignPtr ptr $ \p -> do
+    let p' = p `plusPtr` off
+    glBindTexture gl_TEXTURE_2D tex
+    glTexImage2D gl_TEXTURE_2D 0 3
+      (fromIntegral w) (fromIntegral h) 0 gl_RGB gl_UNSIGNED_BYTE
+      p'
+    let glLinear = fromIntegral gl_LINEAR
+    glTexParameteri gl_TEXTURE_2D gl_TEXTURE_MIN_FILTER glLinear
+    glTexParameteri gl_TEXTURE_2D gl_TEXTURE_MAG_FILTER glLinear
+  return tex
+
+resizeScene :: GLFW.WindowSizeCallback
+resizeScene win w     0      = resizeScene win w 1 -- prevent divide by zero
+resizeScene _   width height = do
+  glViewport 0 0 (fromIntegral width) (fromIntegral height)
+  glMatrixMode gl_PROJECTION
+  glLoadIdentity
+  gluPerspective 45 (fromIntegral width/fromIntegral height) 0.1 100 
+  glMatrixMode gl_MODELVIEW
+  glLoadIdentity
+  glFlush
+
+drawScene :: GLuint -> GLFW.Window -> IO ()
+drawScene tex _ = do
+  -- clear the screen and the depth buffer
+  glClear $ fromIntegral  $  gl_COLOR_BUFFER_BIT
+                         .|. gl_DEPTH_BUFFER_BIT
+  glLoadIdentity  -- reset view
+  glTranslatef 0 0 (-10.0)
+  glBindTexture gl_TEXTURE_2D tex  
+  glBegin gl_QUADS
+  glTexCoord2f   0    0
+  glVertex3f   (-1) (-1)    1  -- bottom left of quad (Front)
+  glTexCoord2f   1    0
+  glVertex3f     1  (-1)    1  -- bottom right of quad (Front)
+  glTexCoord2f   1    1 
+  glVertex3f     1    1     1  -- top right of quad (Front)
+  glTexCoord2f   0    1 
+  glVertex3f   (-1)   1     1  -- top left of quad (Front)
+  glTexCoord2f   1    0 
+  glVertex3f   (-1) (-1) (-1)  -- bottom right of quad (Back)
+  glTexCoord2f   1    1 
+  glVertex3f   (-1)   1  (-1)  -- top right of quad (Back)
+  glTexCoord2f   0    1 
+  glVertex3f     1    1  (-1)  -- top left of quad (Back)
+  glTexCoord2f   0    0 
+  glVertex3f     1  (-1) (-1)  -- bottom left of quad (Back)
+  
+  glEnd
+  glFlush
+
+shutdown :: GLFW.WindowCloseCallback
+shutdown win = do
+  GLFW.destroyWindow win
+  GLFW.terminate
+  _ <- exitWith ExitSuccess
+  return ()
+
+keyPressed :: GLFW.KeyCallback 
+keyPressed win GLFW.Key'Escape _ GLFW.KeyState'Pressed _ = shutdown win
+keyPressed _   _               _ _                     _ = return ()
+
+main :: IO ()
+main = do
+     True <- GLFW.init
+     GLFW.defaultWindowHints
+     -- get a 640 x 480 window
+     -- initialize our window.
+     Just win <- GLFW.createWindow 800 600 "A Bridge Far Away..." Nothing Nothing
+
+     GLFW.makeContextCurrent (Just win)
+     tex <- initGL win
+     -- register the function to do all our OpenGL drawing
+     GLFW.setWindowRefreshCallback win (Just (drawScene tex))
+     -- register the funciton called when our window is resized
+     GLFW.setFramebufferSizeCallback win (Just resizeScene)
+     -- register the function called when the keyboard is pressed.
+     GLFW.setKeyCallback win (Just keyPressed)
+     -- register window close handler
+     GLFW.setWindowCloseCallback win (Just shutdown)
+     -- start event processing engine
+     forever $ do
+       GLFW.pollEvents
+       drawScene tex win
+       GLFW.swapBuffers win
